07e3482f5a8069b4e508e16e08e523d5e510b719
minghui <342015908@qq.com>
2019-04-04 15:48:20 +0800

    消息队列配置，精简代码

--- ocloud-config/src/main/java/com/ocloudwork/cloud/config/CloudConfigApplication.java
+++ ocloud-config/src/main/java/com/ocloudwork/cloud/config/CloudConfigApplication.java
@@ -1,7 +1,5 @@
 package com.ocloudwork.cloud.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -10,9 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class CloudConfigApplication {
 	
-	private static Logger log=LoggerFactory.getLogger(CloudConfigApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigApplication.class, args);
-		log.error("启动完成！");
 	}
 }
