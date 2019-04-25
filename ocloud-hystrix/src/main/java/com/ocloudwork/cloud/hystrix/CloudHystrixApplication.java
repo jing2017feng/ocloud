56281e7566d4e17631aa6335d250e1703d6cb51c
minghui <342015908@qq.com>
2019-04-25 15:18:37 +0800

    配置文件

--- ocloud-hystrix/src/main/java/com/ocloudwork/cloud/hystrix/CloudHystrixApplication.java
+++ ocloud-hystrix/src/main/java/com/ocloudwork/cloud/hystrix/CloudHystrixApplication.java
@@ -2,14 +2,10 @@ package com.ocloudwork.cloud.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableEurekaClient
 @EnableHystrixDashboard
 @SpringBootApplication
-@EnableTurbine
 public class CloudHystrixApplication {
 
 	public static void main(String[] args) {
