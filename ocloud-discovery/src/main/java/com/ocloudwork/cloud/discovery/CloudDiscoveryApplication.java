14002ba27d8cbd9b2b4637f068f0afba9e93c32b
minghui <342015908@qq.com>
2019-04-26 10:13:10 +0800

    配置优化

--- ocloud-discovery/src/main/java/com/ocloudwork/cloud/discovery/CloudDiscoveryApplication.java
+++ ocloud-discovery/src/main/java/com/ocloudwork/cloud/discovery/CloudDiscoveryApplication.java
@@ -2,12 +2,10 @@ package com.ocloudwork.cloud.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@RefreshScope
 public class CloudDiscoveryApplication {
 
 	public static void main(String[] args) {
